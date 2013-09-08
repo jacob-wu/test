90ee4611d282a0a87375ed91ddeff6c2f9863e93
Jacob Wu <Jacob@Jacobs-MacBook-Air.local>
2013-09-08 16:19:31 -0700

    Finish demo app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-  # To change this template use File | Settings | File Templates.
-  validates_presence_of :user_name
+  attr_accessible :email, :name
+
+  has_many :microposts
 end
